80124f6a787e9d211bcbff2e25e301780de79c9d
Julia Nikishina <pandora55592@mail.ru>
2022-03-24 13:34:45 +0300

    Добавление кнопки.

--- app/src/main/java/ru/gb/a2kotlin/MainActivity.kt
+++ app/src/main/java/ru/gb/a2kotlin/MainActivity.kt
@@ -2,11 +2,23 @@ package ru.gb.a2kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import ru.gb.a2kotlin.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var closeMe: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        closeMe = findViewById(R.id.close_me)
+
+        closeMe.setOnClickListener {
+            Log.i("MainActivity", "Hello World")
+        }
     }
 }
