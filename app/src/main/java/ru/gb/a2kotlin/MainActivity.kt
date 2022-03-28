33a06f5a283d1d3e5793879c9e139ee9e667ef1f
Julia Nikishina <pandora55592@mail.ru>
2022-03-28 15:33:04 +0300

    Тренировка в Kotlin.

--- app/src/main/java/ru/gb/a2kotlin/MainActivity.kt
+++ app/src/main/java/ru/gb/a2kotlin/MainActivity.kt
@@ -5,20 +5,68 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import ru.gb.a2kotlin.R
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var closeMe: Button
+    lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         closeMe = findViewById(R.id.close_me)
+        textView = findViewById(R.id.textView)
+
 
         closeMe.setOnClickListener {
             Log.i("MainActivity", "Hello World")
+            textView.text = ""
+            for(i in 10 downTo 1 step 2) {
+                textView.append("Hello Kotlin!")
+            }
+            test()
         }
+
+        val person = Person("Misha", 23 )
+
+        person.copy("Alex")
+
+        fun somePerson() {
+            textView.append(person.firstName)
+            textView.append(" ")
+            textView.append(person.someVal.toString())
+        }
+
+        somePerson();
+
+
+
+
     }
+
+    fun test() {
+        val person = Person("Misha", 23 )
+        val personList: List<Person> = arrayListOf(person, Person("Oleg", 43))
+        for (personVal in personList) {
+            Log.i("MainActivity1",personVal.firstName)
+        }
+
+        for (i in 1..10) {
+            Log.i("MainActivity2","Hello Kotlin!")
+        }
+
+        for (i in 10 downTo 1 step 2) {
+            Log.i("MainActivity3","Hello Kotlin!")
+        }
+
+        for (i in 0 until personList.size) {
+            Log.i("MainActivity4", personList[i].age.toString())
+        }
+
+    }
+
+
 }
